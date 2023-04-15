b0722c2ab5b8d986a99a76c2076171e4f54f1e90
Manuel <saveniamanuel@gmail.com>
2023-04-15 12:47:48 -0300

    Se termino la practica 5

--- Practica5/B/Main.java
+++ Practica5/B/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArbolGeneral<Empleado> a = new ArbolGeneral<Empleado>(new Empleado("Juan", 10, 1));
         a.agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Pedro", 5, 2)));
         a.agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Maria", 15, 2)));
@@ -7,8 +7,10 @@ public class Main {
         a.getHijos().elemento(1).agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Ana", 10, 3)));
         a.getHijos().elemento(2).agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Luis", 5, 3)));
         a.getHijos().elemento(2).agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Marta", 15, 3)));
-        a.getHijos().elemento(1).getHijos().elemento(0).agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Pablo", 10, 4)));
-        a.getHijos().elemento(1).getHijos().elemento(0).agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Sofia", 5, 4)));
+        a.getHijos().elemento(1).getHijos().elemento(0)
+                .agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Pablo", 10, 4)));
+        a.getHijos().elemento(1).getHijos().elemento(0)
+                .agregarHijo(new ArbolGeneral<Empleado>(new Empleado("Sofia", 5, 4)));
 
         Empresa e = new Empresa(a);
         System.out.println(e.empleadosPorCategoria(2));
@@ -23,10 +25,8 @@ public class Main {
         System.out.println();
         e.imprimirporNivel();
 
-
-
         RedDeAguaPotable red = new RedDeAguaPotable();
-        ArbolGeneral<Double> arbol = new ArbolGeneral<Double>(null); 
+        ArbolGeneral<Double> arbol = new ArbolGeneral<Double>(null);
         arbol.agregarHijo(new ArbolGeneral<Double>(null));
         arbol.agregarHijo(new ArbolGeneral<Double>(null));
         arbol.agregarHijo(new ArbolGeneral<Double>(null));
@@ -40,6 +40,17 @@ public class Main {
         System.out.println();
         System.out.println(red.minimoCaudal(arbol, 4500.0));
 
+        Tries trie = new Tries();
+        trie.agregarPalabra("arbol");
+        trie.agregarPalabra("araña");
+        trie.agregarPalabra("arañita");
+        trie.agregarPalabra("arar");
+        trie.agregarPalabra("hola");
+        trie.agregarPalabra("hoja");
+        trie.agregarPalabra("homo");
+        trie.imprimirporNivel();
+        System.out.println();
+        System.out.println(trie.palabrasQueEmpiezanCon("h"));
 
     }
 
---
+++ Practica5/B/Tries.java
@@ -0,0 +1,111 @@
+public class Tries {
+    private ArbolGeneral<Character> trie;
+
+    public Tries() {
+        this.trie = new ArbolGeneral<Character>(null);
+    }
+
+    public Tries(ArbolGeneral<Character> trie) {
+        this.trie = trie;
+    }
+
+    public ArbolGeneral<Character> agregarPalabra(String palabra) {
+        ArbolGeneral<Character> arbol_aux = this.trie;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            Character aux = palabra.charAt(i);
+            ListaGenerica<ArbolGeneral<Character>> hijos = arbol_aux.getHijos();
+            hijos.comenzar();
+
+            for (int k = 0; k < hijos.tamanio(); k++) { // agrego un hijo nuevo y muevo los datos de todos los//
+
+                if (hijos.elemento(k).getDato() == aux) {
+                    arbol_aux = hijos.elemento(k);
+                    break;
+                }
+                if (hijos.elemento(k).getDato() > aux) {
+                    hijos.agregarEn(new ArbolGeneral<Character>(aux), k);
+                    arbol_aux = hijos.elemento(k);
+                    break;
+                }
+                if (k == hijos.tamanio() - 1) {
+                    hijos.agregarFinal(new ArbolGeneral<Character>(aux));
+                    arbol_aux = hijos.elemento(k + 1);
+                    break;
+                }
+            }
+            if (hijos.esVacia()) {
+                hijos.agregarInicio(new ArbolGeneral<Character>(aux));
+                arbol_aux = hijos.elemento(0);
+
+            }
+
+        }
+
+        return arbol_aux;
+    }
+
+    public ListaGenerica<StringBuilder> palabrasQueEmpiezanCon(String prefijo) {
+        ListaGenerica<ArbolGeneral<Character>> hijos = this.trie.getHijos();
+        ListaGenerica<StringBuilder> lista = new ListaGenericaEnlazada<StringBuilder>();
+        hijos.comenzar();
+        for (int i = 0; i < prefijo.length(); i++) {
+            Character aux = prefijo.charAt(i);
+            for (int k = 0; k < hijos.tamanio(); k++) {
+                if (hijos.elemento(k).getDato() == aux) {
+                    hijos = hijos.elemento(k).getHijos();
+                    hijos.comenzar();
+                    break;
+                } else if (k == hijos.tamanio() - 1) {
+                    System.out.println("entro null");
+                    return null;
+                }
+            }
+        }
+
+        StringBuilder aux = new StringBuilder(prefijo);
+        this.palabrasQueEmpiezanCon(lista, hijos, aux);
+
+        return lista;
+    }
+
+    private void palabrasQueEmpiezanCon(ListaGenerica<StringBuilder> lista,
+            ListaGenerica<ArbolGeneral<Character>> hijos, StringBuilder prefijo) {
+        hijos.comenzar();
+        for (int i = 0; i < hijos.tamanio(); i++) {
+            prefijo.append(hijos.elemento(i).getDato());
+            System.out.println(prefijo);
+            if (hijos.elemento(i).tieneHijos()) {
+                this.palabrasQueEmpiezanCon(lista, hijos.elemento(i).getHijos(), prefijo);
+            } else {
+                System.out.println("es hoja");
+                lista.agregarFinal(new StringBuilder(prefijo));
+            }
+            prefijo.deleteCharAt(prefijo.length() - 1);
+        }
+    }
+
+    public void imprimirporNivel() {
+        Cola<ArbolGeneral<Character>> cola = new Cola<ArbolGeneral<Character>>();
+        ArbolGeneral<Character> arbol_aux;
+        cola.encolar(this.trie);
+        cola.encolar(null);
+        while (!cola.esVacia()) {
+            arbol_aux = cola.desencolar();
+            if (!(arbol_aux == null)) {
+                System.out.print(arbol_aux.getDato() + " ");
+                if (arbol_aux.tieneHijos()) {
+                    ListaGenerica<ArbolGeneral<Character>> hijos = arbol_aux.getHijos();
+                    hijos.comenzar();
+                    while (!hijos.fin()) {
+                        cola.encolar(hijos.proximo());
+                    }
+                }
+            } else if (!cola.esVacia()) {
+                cola.encolar(null);
+                System.out.println();
+            }
+        }
+    }
+
+}
